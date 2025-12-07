31368269b458526ab748da5c55a44f1844e70a81
Arvind PJ <arvindpunk@gmail.com>
2025-12-07 19:42:59 +0530

    solve: day1b

--- day1.sql
+++ day1.sql
@@ -12,4 +12,26 @@ SELECT
         (50 :: Int64, 0 :: UInt64)
     ).2
 FROM
-    "table"
+    "table";
+
+--part b
+SELECT
+    arrayFold(
+        acc,
+        el -> (
+            (acc.1 + el + 100 * 1000000) % 100 :: Int64,
+            acc.2 + multiIf(
+                acc.1 + el >= 100,
+                intDiv(acc.1 + el, 100),
+                acc.1 + el <= 0,
+                IF(acc.1 = 0, 0, 1) + intDiv(-1 * (acc.1 + el), 100),
+                0
+            )
+        ),
+        groupArray(
+            toInt64(replaceOne(replaceOne(line, 'L', '-'), 'R', ''))
+        ),
+        (50 :: Int64, 0 :: Int64)
+    ).2
+FROM
+    "table";
